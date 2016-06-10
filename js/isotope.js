e86422066e20121fa2104dfe9ad3748215d3b934
Gregory Swanson <gswanson@gregorys-mbp.fios-router.home>
2016-06-10 17:20:06 -0400

    css

--- js/isotope.js
+++ js/isotope.js
@@ -36,7 +36,7 @@
     for (var i = 0; i < portfolioimg.length; i++){
 
     var $newitems = 
-            $('<div class="col-sm-6 col-md-3 col-lg-3 ' + portfolioimg[i].imgClass + '"><div class="portfolio-item"><div class="hover-bg"><a href="#"><div class="hover-text"><h4>' + portfolioimg[i].noteone + '</h4><small>' + portfolioimg[i].notetwo + '</small><div class="clearfix"></div><i class="fa fa-plus"></i></div><img src="' + portfolioimg[i].cover + '" class="img-responsive darken" alt="' + portfolioimg[i].noteone + '"></a></div></div>');
+            $('<div class="col-sm-6 col-md-3 col-lg-3 ' + portfolioimg[i].imgClass + '"><div class="portfolio-item"><div class="hover-bg"><a href="#"><div class="hover-text"><h4>' + portfolioimg[i].noteone + '</h4><small>' + portfolioimg[i].notetwo + '</small><div class="clearfix"></div><i class="fa fa-external-link-square"></i></div><img src="' + portfolioimg[i].cover + '" class="img-responsive darken" alt="' + portfolioimg[i].noteone + '"></a></div></div>');
         $isogrid.isotope( 'insert', $newitems );
 
     // var $newitems = 
