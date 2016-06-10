5125fe646fbe2c79ad560f394d311e5c5b821ebd
Gregory Swanson <gswanson@gregorys-mbp.fios-router.home>
2016-06-10 02:10:18 -0400

    inital files

---
+++ js/isotope.js
@@ -0,0 +1,54 @@
+/*====================================
+    Portfolio Isotope Filter
+    ======================================*/
+    $(window).load(function() {
+        var $isogrid = $('#lightbox');
+        
+        $isogrid.isotope({
+            filter: '*',
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        $('.cat a').click(function() {
+            $('.cat .active').removeClass('active');
+            $(this).addClass('active');
+            var selector = $(this).attr('data-filter');
+            $isogrid.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+            });
+            return false;
+        });
+
+ 
+
+// Populate Iso grid
+
+    $("#lightbox").html("");
+    
+    for (var i = 0; i < portfolioimg.length; i++){
+
+    var $newitems = 
+            $('<div class="col-sm-6 col-md-3 col-lg-3 ' + portfolioimg[i].imgClass + '"><div class="portfolio-item"><div class="hover-bg"><a href="#"><div class="hover-text"><h4>' + portfolioimg[i].noteone + '</h4><small>' + portfolioimg[i].notetwo + '</small><div class="clearfix"></div><i class="fa fa-plus"></i></div><img src="' + portfolioimg[i].cover + '" class="img-responsive darken" alt="' + portfolioimg[i].noteone + '"></a></div></div>');
+        $isogrid.isotope( 'insert', $newitems );
+
+    // var $newitems = 
+    //         $('<div class="col-sm-6 col-md-3 col-lg-3' + portfolioimg[i].imgClass + '"><div class="portfolio-item" style="background-image: url(' + portfolioimg[i].cover + ')" alt="' + portfolioimg[i].noteone + '"><div class="hover-bg"><a href="#"><div class="hover-text"><h4>' + portfolioimg[i].noteone + '</h4><small>' + portfolioimg[i].notetwo + '</small><div class="clearfix"></div><i class="fa fa-plus"></i></div></a></div></div>');
+    //     $isogrid.isotope( 'insert', $newitems );
+
+
+    };
+
+   });
+
+
+
+
+               
---
+++ js/portfolioimg.js
@@ -0,0 +1,183 @@
+var portfolioimg = [
+	{
+	id: 1,
+	imgClass: 'web',
+	cover: 'img/portfolio/musicality.png',
+	noteone: 'Musicality',
+	notetwo: 'musician search app',
+	popup: 'xxxx',
+	desc: 'xxxx'
+	},
+	{
+	id: 2,
+	imgClass: 'web',
+	cover: 'img/portfolio/within.png',
+	noteone: 'Within Reach',
+	notetwo: 'job search app',
+	popup: 'xxxx',
+	desc: 'xxxx'
+	},
+	{
+	id: 3,
+	imgClass: 'web',
+	cover: 'img/portfolio/datascraper.png',
+	noteone: 'Data Scraper',
+	notetwo: 'data scraping app',
+	popup: 'xxxx',
+	desc: 'xxxx'
+	},
+	{
+	id: 4,
+	imgClass: 'web',
+	cover: 'img/portfolio/giphy.png',
+	noteone: 'Giphy Movie Search',
+	notetwo: 'search giphy API',
+	popup: 'xxxx',
+	desc: 'xxxx'
+	},
+	{
+	id: 5,
+	imgClass: 'web',
+	cover: 'img/portfolio/seinfeld.png',
+	noteone: 'Seinfeld Trivia',
+	notetwo: 'trivia app',
+	popup: 'xxxx',
+	desc: 'xxxx'
+	},
+	{
+	id: 6,
+	imgClass: 'web',
+	cover: 'img/portfolio/crystal.png',
+	noteone: 'Crystal Collector',
+	notetwo: 'game',
+	popup: 'xxxx',
+	desc: 'xxxx'
+	},
+	{
+	id: 7,
+	imgClass: 'web',
+	cover: 'img/portfolio/bff.png',
+	noteone: 'Find Your New BFF',
+	notetwo: 'survey app',
+	popup: 'xxxx',
+	desc: 'xxxx'
+	},
+	{
+	id: 8,
+	imgClass: 'web',
+	cover: 'img/portfolio/hangman.png',
+	noteone: 'Hangman Punk Edition',
+	notetwo: 'hangman game',
+	popup: 'xxxx',
+	desc: 'xxxx'
+	},
+	{
+	id: 10,
+	imgClass: 'graphic',
+	cover: 'img/portfolio/twentyeight.png',
+	noteone: 'Brochure',
+	notetwo: '28 page brochure',
+	popup: 'xxxx',
+	desc: 'xxxx'
+	},
+	{
+	id: 11,
+	imgClass: 'graphic',
+	cover: 'img/portfolio/eightpage.png',
+	noteone: 'Brochure',
+	notetwo: '8 page brochure',
+	popup: 'xxxx',
+	desc: 'xxxx'
+	},
+	{
+	id: 12,
+	imgClass: 'graphic',
+	cover: 'img/portfolio/record.png',
+	noteone: 'Record Artwork',
+	notetwo: '7\" record',
+	popup: 'xxxx',
+	desc: 'xxxx'
+	},
+	{
+	id: 13,
+	imgClass: 'graphic',
+	cover: 'img/portfolio/musicposter.png',
+	noteone: 'Music Posters',
+	notetwo: 'various',
+	popup: 'xxxx',
+	desc: 'xxxx'
+	},
+	{
+	id: 14,
+	imgClass: 'graphic',
+	cover: 'img/portfolio/trifold.png',
+	noteone: 'Tri-Fold Brochure',
+	notetwo: 'brochure',
+	popup: 'xxxx',
+	desc: 'xxxx'
+	},
+	{
+	id: 15,
+	imgClass: 'graphic',
+	cover: 'img/portfolio/ad.png',
+	noteone: 'Advertisements',
+	notetwo: 'various',
+	popup: 'xxxx',
+	desc: 'xxxx'
+	},
+	{
+	id: 16,
+	imgClass: 'graphic',
+	cover: 'img/portfolio/screen.png',
+	noteone: 'Landing Pages',
+	notetwo: 'virtual conference',
+	popup: 'xxxx',
+	desc: 'xxxx'
+	},
+	{
+	id: 17,
+	imgClass: 'graphic',
+	cover: 'img/portfolio/shirt.png',
+	noteone: 'T-Shirt Designs',
+	notetwo: 'various',
+	popup: 'xxxx',
+	desc: 'xxxx'
+	},
+	{
+	id: 18,
+	imgClass: 'graphic',
+	cover: 'img/portfolio/rollup.png',
+	noteone: 'Roll-up Screens',
+	notetwo: 'trade show',
+	popup: 'xxxx',
+	desc: 'xxxx'
+	},
+	{
+	id: 19,
+	imgClass: 'graphic',
+	cover: 'img/portfolio/logo.png',
+	noteone: 'Logos',
+	notetwo: 'various',
+	popup: 'xxxx',
+	desc: 'xxxx'
+	},
+	{
+	id: 20,
+	imgClass: 'graphic',
+	cover: 'img/portfolio/facebook.png',
+	noteone: 'Facebook',
+	notetwo: 'company page',
+	popup: 'xxxx',
+	desc: 'xxxx'
+	},
+	{
+	id: 20,
+	imgClass: 'graphic',
+	cover: 'img/portfolio/various.png',
+	noteone: 'Various',
+	notetwo: 'various pieces',
+	popup: 'xxxx',
+	desc: 'xxxx'
+	}
+
+];
