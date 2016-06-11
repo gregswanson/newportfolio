08c5ab56e1d0f0d4347bbed26af53f6bc91af479
Gregory Swanson <gswanson@gregorys-mbp.fios-router.home>
2016-06-11 00:42:55 -0400

    portfolio

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,26 @@
 /////Open Modal
 $('#lightbox').on( 'click', '.hover-text', function() {
 	var modalID = this.id;
-	alert(modalID);
+	//alert(modalID);
 	$('#modalImages').empty();
 	$('#modalTitle').empty();
+	$('#modalDes').empty();
+	$('#modalTech').empty();
 	// var url = $(this).data('youtube');
 	// var level = myUsers2[modalID].level;
    $('#myModal').modal('show');
 
    $('#modalTitle').html('<h2 class="modalText">' + portfolioimg[modalID].noteone +'</h2>');
+   $('#modalSubTitle').html('<h3 class="modalText">' + portfolioimg[modalID].notetwo +'</h3>');
+   $('#modalDes').html('<p class="modalText">' + portfolioimg[modalID].desc +'</p>');
+   $('#modalTech').html('<p class="modalText">' + portfolioimg[modalID].tech +'</p>');
+
+   if(portfolioimg[modalID].site){
+   	$('#modalSite').html('<a href="'+ portfolioimg[modalID].site +'"  target="_blank" class="modalText">Visit the site  <span class="fa fa-external-link-square"></span></a>');
+   } else {
+   	$('#modalSite').empty();
+   }
+
    for(var i = 0; i < portfolioimg[modalID].imgs.length; i++) {
    	$('#modalImages').append('<div><img src="' + portfolioimg[modalID].imgs[i] + '" class=" modalImage image-responsive"></div>');
    };
--- js/portfolioimg.js
+++ js/portfolioimg.js
@@ -6,7 +6,10 @@ var portfolioimg = [
 	noteone: 'Musicality',
 	notetwo: 'musician search app',
 	popup: 'xxxx',
-	desc: 'xxxx'
+	imgs: ['img/portfolio/musicality1.png', 'img/portfolio/musicality2.png'],
+	desc: 'Musicality is an application for finding and contacting like-minded musicians in a geographic area. The applications allows users to register for an account, search through registered users, and eventually meet up and jam using a private method for email contact.',
+	tech: 'Technologies used: HTML5/CSS3, JavaScript, JQuery, Bootstrap, Node.js, Express, MySQL, Google Maps API',
+	site: 'https://musicalityapp.herokuapp.com/'
 	},
 	{
 	id: 2,
@@ -15,7 +18,10 @@ var portfolioimg = [
 	noteone: 'Within Reach',
 	notetwo: 'job search app',
 	popup: 'xxxx',
-	desc: 'xxxx'
+	imgs: ['img/portfolio/withinreach1.png', 'img/portfolio/withinreach2.png'],
+	desc: 'WithinReach is a geolocation job search app.',
+	tech: 'Technologies used: HTML5/CSS3, JavaScript, JQuery, Bootstrap, Firebase, Google Maps API, Indeed API',
+	site: 'http://45.63.12.88/withinreach/'
 	},
 	{
 	id: 3,
@@ -24,16 +30,22 @@ var portfolioimg = [
 	noteone: 'Data Scraper',
 	notetwo: 'data scraping app',
 	popup: 'xxxx',
-	desc: 'xxxx'
+	imgs: ['img/portfolio/datascraper1.png'],
+	desc: 'Datascraper is a data scraping app that uses Cheerio. It allows you to add or remove notes associated with the article displayed.',
+	tech: 'Technologies used: HTML5/CSS3, JavaScript, JQuery, Bootstrap, MongoDB, Mongoose, Cheerio',
+	site: 'http://datascraper.herokuapp.com/'
 	},
 	{
 	id: 4,
 	imgClass: 'web',
 	cover: 'img/portfolio/giphy.png',
-	noteone: 'Giphy Movie Search',
+	noteone: 'Giphy Search',
 	notetwo: 'search giphy API',
 	popup: 'xxxx',
-	desc: 'xxxx'
+	imgs: ['img/portfolio/giphysearch1.png'],
+	desc: 'Giphy Movie Search allows you to search the Giphy API. You can use either the predefined search or search for any movie that you want.',
+	tech: 'Technologies used: HTML5/CSS3, JavaScript, JQuery, Bootstrap, MongoDB, Giphy API',
+	site: 'http://pure-inlet-61519.herokuapp.com/'
 	},
 	{
 	id: 5,
@@ -42,7 +54,10 @@ var portfolioimg = [
 	noteone: 'Seinfeld Trivia',
 	notetwo: 'trivia app',
 	popup: 'xxxx',
-	desc: 'xxxx'
+	imgs: ['img/portfolio/trivia.png'],
+	desc: 'Test your knowledge of Seinfeld with this timed trivia game.',
+	tech: 'Technologies used: HTML5/CSS3, JavaScript, JQuery, Bootstrap',
+	site: 'http://desolate-citadel-87593.herokuapp.com/'
 	},
 	{
 	id: 6,
@@ -51,7 +66,10 @@ var portfolioimg = [
 	noteone: 'Crystal Collector',
 	notetwo: 'game',
 	popup: 'xxxx',
-	desc: 'xxxx'
+	imgs: ['img/portfolio/crystal1.png'],
+	desc: 'You will be given a random number at the start of the game. There are four crystals below. By clicking on a crystal you will add a specific amount of points to your total score. You win the game by matching your total score to the random number. You lose the game if your score is higher than the random number. The value of each crystal is hidden until you click on it. Each time the game starts, the value of the crystal will change.',
+	tech: 'Technologies used: HTML5/CSS3, JavaScript, JQuery, Bootstrap',
+	site: 'http://salty-waters-84219.herokuapp.com/'
 	},
 	{
 	id: 7,
@@ -60,16 +78,22 @@ var portfolioimg = [
 	noteone: 'Find Your New BFF',
 	notetwo: 'survey app',
 	popup: 'xxxx',
-	desc: 'xxxx'
+	imgs: ['img/portfolio/bff1.png'],
+	desc: 'This survey will connect you with your closest match based on your answers to the questions',
+	tech: 'Technologies used: HTML5/CSS3, JavaScript, JQuery, Bootstrap, MySQL, Sequelize',
+	site: 'http://bfffinder.herokuapp.com'
 	},
 	{
 	id: 8,
 	imgClass: 'web',
 	cover: 'img/portfolio/hangman.png',
-	noteone: 'Hangman Punk Edition',
+	noteone: 'Hangman',
 	notetwo: 'hangman game',
 	popup: 'xxxx',
-	desc: 'xxxx'
+	imgs: ['img/portfolio/hangman1.png'],
+	desc: 'This is a hangman game with a punk theme. Use the clues to guess the band. If you match all of the letters, you will hear a song from that artist.',
+	tech: 'Technologies used: HTML5/CSS3, JavaScript, Bootstrap',
+	site: 'http://agile-reef-47880.herokuapp.com/hangman.html'
 	},
 	{
 	id: 10,
@@ -79,7 +103,8 @@ var portfolioimg = [
 	notetwo: '28 page brochure',
 	popup: 'xxxx',
 	imgs: ['http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9c6b7e4b08c1a725d4f53/53a9c6d0e4b0ba352f67c940/1403635409434/Xiaflex_2_4_mu.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9c6b7e4b08c1a725d4f53/53a9c6d0e4b08c1a725d4f82/1403635409528/Xiaflex_2_3_mu.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9c6b7e4b08c1a725d4f53/53a9c6d0e4b08c1a725d4f84/1403635409299/Xiaflex_2_2_mu.jpg'],
-	desc: 'xxxx'
+	desc: '28-page brochure for a pharmaceutical client.',
+	tech: 'Technologies used: Photoshop, Illustrator, InDesign'
 	},
 	{
 	id: 11,
@@ -88,7 +113,9 @@ var portfolioimg = [
 	noteone: 'Brochure',
 	notetwo: '8 page brochure',
 	popup: 'xxxx',
-	desc: 'xxxx'
+	imgs: ['http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9c7a3e4b0de487b5a84e3/53a9c878e4b0918746e034a2/1403635875457/SIUH1a_mu2.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9c7a3e4b0de487b5a84e3/53a9c878e4b0ac3522f5f797/1403635858602/SIUH2_mu2.jpg'],
+	desc: 'Informational booklet describing hospital\'s new emergency and trauma center.',
+	tech: 'Technologies used: Photoshop, Illustrator, InDesign'
 	},
 	{
 	id: 12,
@@ -98,7 +125,8 @@ var portfolioimg = [
 	notetwo: '7\" record',
 	popup: 'xxxx',
 	imgs: ['http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9cf7ee4b07db2cfa19bfe/53a9cf9ce4b077fee360a24e/1403637682822/TAJ1.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9cf7ee4b07db2cfa19bfe/53a9cf9ce4b077fee360a250/1403637669201/TAJ2.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9cf7ee4b07db2cfa19bfe/53a9cf9ce4b07b097d155c87/1403637668865/VelocityAmberCVR.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9cf7ee4b07db2cfa19bfe/53a9cf9ce4b07b097d155c89/1403637681638/Label_Art.jpg'],
-	desc: 'xxxx'
+	desc: 'Split 7\" vinyl record for Velocity and The Amber Jets released in US and Japan',
+	tech: 'Technologies used: Photoshop, Illustrator, InDesign'
 	},
 	{
 	id: 13,
@@ -107,26 +135,30 @@ var portfolioimg = [
 	noteone: 'Music Posters',
 	notetwo: 'various',
 	popup: 'xxxx',
-	desc: 'xxxx'
+	imgs: ['http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9c8f0e4b08c1a725d5427/53a9c916e4b03de9feee1fad/1403635991476/Japanposter2C_mu.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9c8f0e4b08c1a725d5427/53a9c917e4b058df6ed1f682/1403635991356/120410_Walter_mu.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9c8f0e4b08c1a725d5427/53a9c961e4b0f2b2922ba1d2/1403636070750/TAJ_DSOD_052209_mu.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9c8f0e4b08c1a725d5427/53a9c918e4b03de9feee1fb5/1403635992764/Sweeps_2_mu.jpg'],
+	desc: 'Collection of posters promoting various live concert events.',
+	tech: 'Technologies used: Photoshop, Illustrator, InDesign'
 	},
-	{
-	id: 14,
-	imgClass: 'graphic',
-	cover: 'img/portfolio/trifold.png',
-	noteone: 'Tri-Fold Brochure',
-	notetwo: 'brochure',
-	popup: 'xxxx',
-	desc: 'xxxx'
-	},
-	{
-	id: 15,
-	imgClass: 'graphic',
-	cover: 'img/portfolio/ad.png',
-	noteone: 'Advertisements',
-	notetwo: 'various',
-	popup: 'xxxx',
-	desc: 'xxxx'
-	},
+	// {
+	// id: 14,
+	// imgClass: 'graphic',
+	// cover: 'img/portfolio/trifold.png',
+	// noteone: 'Tri-Fold Brochure',
+	// notetwo: 'brochure',
+	// popup: 'xxxx',
+	// imgs: [],
+	// desc: 'xxxx'
+	// },
+	// {
+	// id: 15,
+	// imgClass: 'graphic',
+	// cover: 'img/portfolio/ad.png',
+	// noteone: 'Advertisements',
+	// notetwo: 'various',
+	// popup: 'xxxx',
+	// imgs: [],
+	// desc: 'xxxx'
+	// },
 	{
 	id: 16,
 	imgClass: 'graphic',
@@ -134,26 +166,30 @@ var portfolioimg = [
 	noteone: 'Landing Pages',
 	notetwo: 'virtual conference',
 	popup: 'xxxx',
-	desc: 'xxxx'
+	imgs: ['http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9cab6e4b09b44e37a526e/53a9cae2e4b05b89323292a7/1403636461054/vcopious_Screens.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9cab6e4b09b44e37a526e/53a9cae3e4b05b89323292a9/1403636452659/vcopious_ECFD.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9cab6e4b09b44e37a526e/53a9cae2e4b051ebda169044/1403636479442/vcopious_Virtual_Map.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9cab6e4b09b44e37a526e/53a9cb55e4b097b2557bac42/1403636609769/vcopious_Education.jpg'],
+	desc: 'Opening screens for virtual conference events.',
+	tech: 'Technologies used: Photoshop, Illustrator'
 	},
-	{
-	id: 17,
-	imgClass: 'graphic',
-	cover: 'img/portfolio/shirt.png',
-	noteone: 'T-Shirt Designs',
-	notetwo: 'various',
-	popup: 'xxxx',
-	desc: 'xxxx'
-	},
-	{
-	id: 18,
-	imgClass: 'graphic',
-	cover: 'img/portfolio/rollup.png',
-	noteone: 'Roll-up Screens',
-	notetwo: 'trade show',
-	popup: 'xxxx',
-	desc: 'xxxx'
-	},
+	// {
+	// id: 17,
+	// imgClass: 'graphic',
+	// cover: 'img/portfolio/shirt.png',
+	// noteone: 'T-Shirt Designs',
+	// notetwo: 'various',
+	// popup: 'xxxx',
+	// imgs: [],
+	// desc: 'xxxx'
+	// },
+	// {
+	// id: 18,
+	// imgClass: 'graphic',
+	// cover: 'img/portfolio/rollup.png',
+	// noteone: 'Roll-up Screens',
+	// notetwo: 'trade show',
+	// popup: 'xxxx',
+	// imgs: [],
+	// desc: 'xxxx'
+	// },
 	{
 	id: 19,
 	imgClass: 'graphic',
@@ -161,7 +197,9 @@ var portfolioimg = [
 	noteone: 'Logos',
 	notetwo: 'various',
 	popup: 'xxxx',
-	desc: 'xxxx'
+	imgs: ['http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9d140e4b0c45df57da649/53aa535ee4b06de1f72320df/1403711561927/student_leadership.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9d140e4b0c45df57da649/53aa535ee4b06de1f72320e1/1403712299814/ieee_HKN_Logo.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9d140e4b0c45df57da649/53aa5588e4b035c356289e7a/1403671950436/alumni_Reconnect.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9d140e4b0c45df57da649/53aa4b38e4b0ffc29c24adc5/1403711717525/the_Heavenly_Tenants.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9d140e4b0c45df57da649/53aa5789e4b0cb8f6cc4c6f9/1403711742151/dsod.jpg?format=1500w', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9d140e4b0c45df57da649/53aa5789e4b05b8379703aea/1403711829036/taj_logo.jpg'],
+	desc: 'A collection of various logos.',
+	tech: 'Technologies used: Photoshop, Illustrator'
 	},
 	{
 	id: 20,
@@ -170,16 +208,20 @@ var portfolioimg = [
 	noteone: 'Facebook',
 	notetwo: 'company page',
 	popup: 'xxxx',
-	desc: 'xxxx'
+	imgs: ['http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9cd68e4b05b8932329713/53a9cd81e4b0afe50f81e77b/1403637131644/IEEE_Facebook_1.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9cd68e4b05b8932329713/53a9cd81e4b05b8932329755/1403637124587/IEEE_Facebook_4.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9cd68e4b05b8932329713/53a9cd82e4b05b8932329757/1403637129386/IEEE_Facebook_3.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9cd68e4b05b8932329713/53a9cd81e4b0afe50f81e779/1403637136844/IEEE_Facebook_2.jpg' ],
+	desc: 'Series of screens to promote company\'s various programs.',
+	tech: 'Technologies used: Photoshop, Illustrator'
 	},
 	{
-	id: 20,
+	id: 21,
 	imgClass: 'graphic',
 	cover: 'img/portfolio/various.png',
 	noteone: 'Various',
 	notetwo: 'various pieces',
 	popup: 'xxxx',
-	desc: 'xxxx'
+	imgs: ['http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9d250e4b0afe50f81f079/53ab264ae4b0b04a4fac5eb1/1403725428438/GIE_Patch_2.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9d250e4b0afe50f81f079/53ab2669e4b0b04a4fac5ee3/1403725450618/Sticker.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9d250e4b0afe50f81f079/550652a5e4b0c3c9c4ca9da1/1426477734398/OHSP_1a_Keep.jpg', 'http://static1.squarespace.com/static/53a9b17de4b0b4cc4829b1cf/53a9d250e4b0afe50f81f079/550652b1e4b0953c7cbebe5d/1426477749626/OHSP_1b_Keep.jpg'],
+	desc: 'A collection of various projects.',
+	tech: 'Technologies used: Photoshop, Illustraton, InDesign'
 	}
 
 ];
